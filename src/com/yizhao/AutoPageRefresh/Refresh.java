36eadade8b0e6e16e7c827e46cbc57b884d036ce
Yi Zhao <yizhao.cs@gmail.com>
2016-10-04 16:29:43 -0700

    1

---
+++ src/com/yizhao/AutoPageRefresh/Refresh.java
@@ -0,0 +1,60 @@
+package com.yizhao.AutoPageRefresh;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/*
+* https://www.tutorialspoint.com/servlets/servlets-auto-refresh.htm
+* http://localhost:8080/Refresh
+* */
+public class Refresh extends HttpServlet {
+
+    // Method to handle GET method request.
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        // Set refresh, autoload time as 5 seconds
+        response.setIntHeader("Refresh", 5);
+
+        // Set response content type
+        response.setContentType("text/html");
+
+        // Get current time
+        Calendar calendar = new GregorianCalendar();
+        String am_pm;
+        int hour = calendar.get(Calendar.HOUR);
+        int minute = calendar.get(Calendar.MINUTE);
+        int second = calendar.get(Calendar.SECOND);
+        if(calendar.get(Calendar.AM_PM) == 0)
+            am_pm = "AM";
+        else
+            am_pm = "PM";
+
+        String CT = hour+":"+ minute +":"+ second +" "+ am_pm;
+
+        PrintWriter out = response.getWriter();
+        String title = "Auto Page Refresh using Servlet";
+        String docType =
+                "<!doctype html public \"-//w3c//dtd html 4.0 " +
+                        "transitional//en\">\n";
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n"+
+                "<body bgcolor=\"#f0f0f0\">\n" +
+                "<h1 align=\"center\">" + title + "</h1>\n" +
+                "<p>Current Time is: " + CT + "</p>\n");
+    }
+    // Method to handle POST method request.
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
---
+++ src/com/yizhao/SendingEmail/SendEmail.java
@@ -0,0 +1,83 @@
+package com.yizhao.SendingEmail;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Properties;
+
+/**
+ * https://www.tutorialspoint.com/servlets/servlets-sending-email.htm
+ * Browser:
+ * http://localhost:8080/SendEmail
+ */
+public class SendEmail extends HttpServlet {
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        // Recipient's email ID needs to be mentioned.
+        String to = "abcd@gmail.com";
+
+        // Sender's email ID needs to be mentioned
+        String from = "web@gmail.com";
+
+        // Assuming you are sending email from localhost
+        String host = "localhost";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.setProperty("mail.smtp.host", host);
+
+        // Get the default Session object.
+        Session session = Session.getDefaultInstance(properties);
+
+        // Set response content type
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        MimeMessage message = null;
+        try{
+            // Create a default MimeMessage object.
+            message = new MimeMessage(session);
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(to));
+            // Set Subject: header field
+            message.setSubject("This is the Subject Line!");
+            // Now set the actual message
+            message.setText("This is actual message");
+            // Send message
+            Transport.send(message);
+            String title = "Send Email";
+            String res = "Sent message successfully....";
+            String docType =
+                    "<!doctype html public \"-//w3c//dtd html 4.0 " +
+                            "transitional//en\">\n";
+            out.println(docType +
+                    "<html>\n" +
+                    "<head><title>" + title + "</title></head>\n" +
+                    "<body bgcolor=\"#f0f0f0\">\n" +
+                    "<h1 align=\"center\">" + title + "</h1>\n" +
+                    "<p align=\"center\">" + res + "</p>\n" +
+                    "</body></html>");
+
+        } catch (AddressException e){
+
+        }catch (javax.mail.MessagingException e){
+            e.printStackTrace();
+        }
+    }
+}
